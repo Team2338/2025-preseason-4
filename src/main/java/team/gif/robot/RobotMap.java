9fbe77c9f27504f9ca70ed6b782af9c1ef7d4668
HexFlame2157 <floweytheflower2157@gmail.com>
2025-11-04 19:18:21 -0800

    Added subsystem and commands

--- src/main/java/team/gif/robot/RobotMap.java
+++ src/main/java/team/gif/robot/RobotMap.java
@@ -5,4 +5,6 @@ public abstract class RobotMap {
     public static final int DRIVER_CONTROLLER_ID = 0;
     public static final int AUX_CONTROLLER_ID = 1;
     public static final int TEST_CONTROLLER_ID = 2;
+    public static final int WINCH_MOTOR_ID = 5;
+    public static final int WINCH_MOTOR_ID_BOTTOM = 0;
 }
