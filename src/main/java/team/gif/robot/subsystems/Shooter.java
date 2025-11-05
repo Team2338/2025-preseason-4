2de01b9fa27170e2f28552a9e90586471e8fb67e
Accidental MIstake <18001870@students.sd308.org>
2025-11-04 20:06:06 -0600

    fixed formating

--- src/main/java/team/gif/robot/subsystems/Shooter.java
+++ src/main/java/team/gif/robot/subsystems/Shooter.java
@@ -1,30 +1,27 @@
 package team.gif.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import team.gif.robot.Constants;
-import team.gif.robot.Robot;
 import team.gif.robot.RobotMap;
 public class Shooter extends SubsystemBase {
 
-    private TalonSRX motortop;
-    private TalonSRX motorbottom;
+    private final TalonSRX motorTop;
+    private final TalonSRX motorBottom;
 
     public Shooter() {
-        motortop = new TalonSRX(RobotMap.WINCH_MOTOR_ID);
-        motortop.configFactoryDefault();
-        motortop.setNeutralMode(NeutralMode.Brake);
-        motorbottom = new TalonSRX(RobotMap.WINCH_MOTOR_ID_BOTTOM);
-        motorbottom.configFactoryDefault();
-        motorbottom.setNeutralMode(NeutralMode.Brake);
+        motorTop = new TalonSRX(RobotMap.WINCH_MOTOR_ID);
+        motorTop.configFactoryDefault();
+        motorTop.setNeutralMode(NeutralMode.Brake);
+        motorBottom = new TalonSRX(RobotMap.WINCH_MOTOR_ID_BOTTOM);
+        motorBottom.configFactoryDefault();
+        motorBottom.setNeutralMode(NeutralMode.Brake);
     }
 
     public void setSpeed(double percentOutputTop, double percentOutputBot) {
-        motortop.set(TalonSRXControlMode.PercentOutput, percentOutputTop);
-        motorbottom.set(TalonSRXControlMode.PercentOutput, percentOutputBot);
+        motorTop.set(ControlMode.PercentOutput, percentOutputTop);
+        motorBottom.set(ControlMode.PercentOutput, percentOutputBot);
     }
 }
 
