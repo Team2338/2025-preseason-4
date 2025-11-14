bede74de94ed602403c202c2aa67db69148957e7
Accidental MIstake <18001870@students.sd308.org>
2025-11-13 19:32:25 -0600

    WIP autobuilder

--- src/main/java/team/gif/robot/subsystems/DriveMotorSystem.java
+++ src/main/java/team/gif/robot/subsystems/DriveMotorSystem.java
@@ -5,6 +5,8 @@
 package team.gif.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
@@ -12,9 +14,14 @@ import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import team.gif.robot.Robot;
 import team.gif.robot.RobotMap;
 
 import static team.gif.robot.Constants.*;
@@ -24,56 +31,59 @@ public class DriveMotorSystem extends SubsystemBase {
     /** Creates a new ExampleSubsystem. */
     public SparkMax sparkleft;
     public SparkMax sparkright;
-    public SparkMaxConfig babsSparkConfigleft;
-    public SparkMaxConfig babsSparkConfigright;
     public RelativeEncoder sparkEncoderleft;
     public RelativeEncoder sparkEncoderright;
-    public SparkClosedLoopController motorloopleft;
-    public SparkClosedLoopController motorloopright;
     public DifferentialDrive drive;
+    public DifferentialDrivePoseEstimator poseEstimator;
+    
+
     public DriveMotorSystem() {
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.inverted(true);
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+
         sparkleft = new SparkMax(SPARK_MAX_ID, SparkLowLevel.MotorType.kBrushless);
         sparkEncoderleft = sparkleft.getEncoder();
-        sparkleft.configure(babsSparkConfigleft, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        babsSparkConfigleft.idleMode(SparkBaseConfig.IdleMode.kBrake);
-        babsSparkConfigleft.inverted(true);
-        motorloopleft = sparkleft.getClosedLoopController();
-        babsSparkConfigleft.closedLoop.p(kP);
-        babsSparkConfigleft.closedLoop.i(kI);
-        babsSparkConfigleft.closedLoop.d(kD);
+        sparkleft.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
 
 
         sparkright = new SparkMax(SPARK_MAX_ID, SparkLowLevel.MotorType.kBrushless);
         sparkEncoderright = sparkright.getEncoder();
-        sparkright.configure(babsSparkConfigright, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        babsSparkConfigright.idleMode(SparkBaseConfig.IdleMode.kBrake);
-        babsSparkConfigright.inverted(true);
-        motorloopright = sparkright.getClosedLoopController();
-        babsSparkConfigright.closedLoop.p(kP);
-        babsSparkConfigright.closedLoop.i(kI);
-        babsSparkConfigright.closedLoop.d(kD);
+        sparkright.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
 
 
         drive = new DifferentialDrive(sparkleft, sparkright);
 
+        RobotConfig robotConfig;
+        try {
+            robotConfig = RobotConfig.fromGUISettings();
+        } catch (Exception e) {
+            // Handle exception as needed
+            e.printStackTrace();
+        }
 
-    }
-    public double velocityGetleft(){
-        return  sparkEncoderleft.getVelocity();
-    }
-    public double velocityGetright() {
+        AutoBuilder.configure(
+                this::getPose,
 
-        return sparkEncoderright.getVelocity();
+
+        );
     }
-    public void setRPM(double RPM){
-        motorloopleft.setReference(RPM, SparkBase.ControlType.kVelocity);
-        motorloopright.setReference(RPM, SparkBase.ControlType.kVelocity);
+
+    public Pose2d getPose(){
+        return poseEstimator.getEstimatedPosition();
     }
+
+//    public double velocityGetleft(){
+//        return  sparkEncoderleft.getVelocity();
+//    }
+//    public double velocityGetright() {
+//
+//        return sparkEncoderright.getVelocity();
+//    }
+
     public void setVoltage(double Voltage){
         sparkright.set(Voltage);
         sparkleft.set(Voltage);
-
-
     }
     public void tankdrive(double leftspeed, double rightspeed){
         drive.tankDrive(leftspeed, rightspeed);
